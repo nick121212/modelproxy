991a25a320cf7526776e5ea17748d0c3be9ee3b5
nick121212 <xiao.feng@weimob.com>
2019-06-12 11:28:58 +0800

    update

--- packages/modelproxy-engine-base/out/index.js
+++ packages/modelproxy-engine-base/out/index.js
@@ -1,4 +1,14 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -7,17 +17,46 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const modelproxy_1 = require("modelproxy");
-class BaseEngine extends modelproxy_1.DefaultEngine {
+var modelproxy_1 = require("modelproxy");
+var BaseEngine = /** @class */ (function (_super) {
+    __extends(BaseEngine, _super);
     /**
      * Creates an instance of MiniGEngine.
      * @param {FetchFunction} fetchFunc
      * @memberof MiniGEngine
      */
-    constructor(fetchFunc) {
-        super();
-        this.fetchFunc = fetchFunc;
+    function BaseEngine(fetchFunc) {
+        var _this = _super.call(this) || this;
+        _this.fetchFunc = fetchFunc;
+        return _this;
     }
     /**
      * 发起请求
@@ -25,23 +64,36 @@ class BaseEngine extends modelproxy_1.DefaultEngine {
      * @param  {(s?: string) => Promise<any>}  next 下一个中间件
      * @return {Promise<any>}
      */
-    fetch(ctx, next) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const { instance = {}, executeInfo = {}, settings = {} } = ctx;
-            const { method } = instance;
-            const url = this.getFullPath(instance, executeInfo);
-            const { timeout } = settings;
-            const { data } = executeInfo;
-            // 使用超时设置
-            ctx.result = yield Promise.race([
-                this.delay(timeout || 5000).then(() => {
-                    throw new modelproxy_1.MPError(`接口请求超时！(${timeout})`, "timeout", ctx);
-                }),
-                this.fetchFunc(url, method, data, ctx)
-            ]);
-            yield next();
+    BaseEngine.prototype.fetch = function (ctx, next) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, instance, _b, executeInfo, _c, settings, method, url, timeout, data, _d;
+            return __generator(this, function (_e) {
+                switch (_e.label) {
+                    case 0:
+                        _a = ctx.instance, instance = _a === void 0 ? {} : _a, _b = ctx.executeInfo, executeInfo = _b === void 0 ? {} : _b, _c = ctx.settings, settings = _c === void 0 ? {} : _c;
+                        method = instance.method;
+                        url = this.getFullPath(instance, executeInfo);
+                        timeout = settings.timeout;
+                        data = executeInfo.data;
+                        // 使用超时设置
+                        _d = ctx;
+                        return [4 /*yield*/, Promise.race([
+                                this.delay(timeout || 5000).then(function () {
+                                    throw new modelproxy_1.MPError("\u63A5\u53E3\u8BF7\u6C42\u8D85\u65F6\uFF01(" + timeout + ")", "timeout", ctx);
+                                }),
+                                this.fetchFunc(url, method, data, ctx)
+                            ])];
+                    case 1:
+                        // 使用超时设置
+                        _d.result = _e.sent();
+                        return [4 /*yield*/, next()];
+                    case 2:
+                        _e.sent();
+                        return [2 /*return*/];
+                }
+            });
         });
-    }
+    };
     /**
      * 发起调用，接口方法
      * @param   {IInterfaceModel} instance      当前接口的实例
@@ -49,12 +101,24 @@ class BaseEngine extends modelproxy_1.DefaultEngine {
      * @param   {any[]}           otherOptions  额外的参数
      * @returns {Promise<any>}
      */
-    proxy(instance, executeInfo, ...otherOptions) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const { result } = yield this.doProxy(instance, executeInfo, ...otherOptions);
-            return result;
+    BaseEngine.prototype.proxy = function (instance, executeInfo) {
+        var otherOptions = [];
+        for (var _i = 2; _i < arguments.length; _i++) {
+            otherOptions[_i - 2] = arguments[_i];
+        }
+        return __awaiter(this, void 0, void 0, function () {
+            var result;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.doProxy.apply(this, [instance, executeInfo].concat(otherOptions))];
+                    case 1:
+                        result = (_a.sent()).result;
+                        return [2 /*return*/, result];
+                }
+            });
         });
-    }
-}
+    };
+    return BaseEngine;
+}(modelproxy_1.DefaultEngine));
 exports.BaseEngine = BaseEngine;
 //# sourceMappingURL=index.js.map
